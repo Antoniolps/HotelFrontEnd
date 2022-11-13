29f26b781f262a6c4bc0ccafde04a61bf5f4feb8
Antonio Lopes <antoniolps218@gmail.com>
2022-11-13 19:39:45 -0300

    Removendo o rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,7 +7,6 @@ import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,6 @@ import { RodapeComponent } from './components/rodape/rodape.component';
     HomeComponent,
     CadastroComponent,
     LoginComponent,
-    RodapeComponent
   ],
   imports: [
     BrowserModule,
