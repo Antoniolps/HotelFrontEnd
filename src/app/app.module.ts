3a4e6349032ab62312e00be40f381a3e2611059e
Antonio Lopes <antoniolps218@gmail.com>
2022-11-13 18:20:49 -0300

    Telas de login e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormComponent
+    FormComponent,
+    HomeComponent,
+    CadastroComponent,
+    LoginComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
